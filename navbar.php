19fd6973a8eb64939af52b61c5e0468ffde88a58
Devine560 <devineboakye560@gmail.com>
2023-03-29 01:03:11 +0000

    changes has been made
    changes to about.html,blog.html,contact.html,events.html,executiveprofile.html,index.html,navbar.php,pastexecutive.html,,services.html,team.html,testimonials.html

---
+++ navbar.php
@@ -0,0 +1,44 @@
+<nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a href="index.html" class="active">Home</a></li>
+
+          <li class="dropdown"><a href="#"><span>About</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="aboutkhoders.html">About Khoders</a></li>
+              <li><a href="ExecutiveProfile.html">Executive Profile</a></li>
+              <li><a href="PastExecutive.html">Past Executive</a></li>
+              <li><a href="contact.html" class="active">Contact</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          
+          
+
+          <li><a href="Events.html">Events</a></li>
+          <!-- <li><a href="portfolio.html">Portfolio</a></li> -->
+          <!-- <li><a href="pricing.html">Pricing</a></li> -->
+          <li><a href="blog.html">Blog</a></li>
+          <!-- <li><a href="contact.html">Contact</a></li> -->
+          <li><a href="#">Sign In</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <div class="header-social-links d-flex">
+        <!-- <a href="#" class="twitter"><i class="bu bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bu bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bu bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bu bi-linkedin"></i></i></a> -->
+        <!-- <div><button class="btn btn-primary">Sign In</button></div>
+        <div><button class="btn btn-success">Sign Up</button></div> -->
+      </div>
+
+    </div>
